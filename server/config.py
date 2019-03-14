86f3f17e8907c52ce50fc4b8c728da13724de302
Joao Meyer <1994meyer@gmail.com>
2019-03-14 01:12:13 -0300

    Added basic config

---
+++ server/config.py
@@ -0,0 +1,16 @@
+import os
+
+# Use to create the local, usually for testing, database
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLITE_MEM = 'sqlite:///:memory'
+
+
+class Config(object):
+    '''
+    Basic configuration objecct for flask instance app.
+    '''
+
+    # The environment variable DATABASE_URL is used in Heroku when setting
+    # their database system
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or SQLITE_MEM
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
